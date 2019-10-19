0920f1813aac645c39d83219c0f86ecb8ae87dab
hello <56299050+crystal-box@users.noreply.github.com>
2019-10-19 22:38:12 +0800

    Add files via upload

---
+++ Practice 11-15.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#!/usr/bin/python3
+ 
+list1 = ['Google', 'Runoob', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+ 
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
+
+#!/usr/bin/python3
+ 
+list = ['Google', 'Runoob', 1997, 2000]
+ 
+print ("第三个元素为 : ", list[2])
+list[2] = 2001
+print ("更新后的第三个元素为 : ", list[2])
+
+#!/usr/bin/python3
+ 
+list = ['Google', 'Runoob', 1997, 2000]
+ 
+print ("原始列表 : ", list)
+del list[2]
+print ("删除第三个元素 : ", list)
+
+#!/usr/bin/python3
+ 
+tup1 = ('Google', 'Runoob', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+ 
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+
+#!/usr/bin/python3
+ 
+tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz')
+ 
+# 以下修改元组元素操作是非法的。
+# tup1[0] = 100
+ 
+# 创建一个新的元组
+tup3 = tup1 + tup2;
+print (tup3)
+
+#!/usr/bin/python3
+ 
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+ 
+print ("dict['Name']: ", dict['Name'])
+print ("dict['Age']: ", dict['Age'])
+
+#!/usr/bin/python3
+ 
+dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+ 
+dict['Age'] = 8               # 更新 Age
+dict['School'] = "菜鸟教程"  # 添加信息
+ 
+ 
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
+
+#!/usr/bin/python3
+ 
+dict = {'Name': 'Runoob', 'Age': 7, 'Name': '小菜鸟'}
+ 
+print ("dict['Name']: ", dict['Name'])
+
+#!/usr/bin/python3
+ 
+# Fibonacci series: 斐波纳契数列
+# 两个元素的总和确定了下一个数
+a, b = 0, 1
+while b < 10:
+    print(b)
+    a, b = b, a+b
+    
+#!/usr/bin/python3
+ 
+# Fibonacci series: 斐波纳契数列
+# 两个元素的总和确定了下一个数
+a, b = 0, 1
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a+b
